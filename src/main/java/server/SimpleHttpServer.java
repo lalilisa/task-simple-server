6de42868aa6bec989e24666b9fe14a472e46c57e
interns.trimv <maivantri309@gmail.com>
2023-02-13 08:58:52 +0700

    refactor

--- src/main/java/server/SimpleHttpServer.java
+++ src/main/java/server/SimpleHttpServer.java
@@ -25,45 +25,55 @@ public class SimpleHttpServer {
     }
     static class MyHandler implements HttpHandler {
         @Override
-        public void handle(HttpExchange t) throws IOException {
-            if(t.getRequestMethod().equalsIgnoreCase("GET")){
+        public void handle(HttpExchange httpExchange) throws IOException {
+            if(httpExchange.getRequestMethod().equalsIgnoreCase("GET")){
                 File file=new File("index.html");
                 FileInputStream fileInputStream=new FileInputStream(file);
+
                 BufferedReader bufferedReader=new BufferedReader(new InputStreamReader(fileInputStream,StandardCharsets.UTF_8));
                 StringBuilder buf=new StringBuilder();
                 String line=null;
+
                 while ((line = bufferedReader.readLine()) != null) {
                     buf.append(line);
                 }
                 String response =buf.toString();
-                Headers h = t.getResponseHeaders();
-                h.put("Content-Type", Collections.singletonList("text/html;charset=UTF-8"));
                 byte[] bytes = response.getBytes(StandardCharsets.UTF_8);
-                t.sendResponseHeaders(200, bytes.length);
-                OutputStream os = t.getResponseBody();
+
+                Headers headerResponse = httpExchange.getResponseHeaders();
+                headerResponse.put("Content-Type", Collections.singletonList("text/html;charset=UTF-8"));
+                httpExchange.sendResponseHeaders(200, bytes.length);
+
+                OutputStream os = httpExchange.getResponseBody();
                 os.write(bytes);
                 os.flush();
                 os.close();
             }
-            if(t.getRequestMethod().equalsIgnoreCase("POST")){
+            if(httpExchange.getRequestMethod().equalsIgnoreCase("POST")){
 
-                BufferedReader in = new BufferedReader(new InputStreamReader(t.getRequestBody(),"UTF-8"));
+                BufferedReader in = new BufferedReader(new InputStreamReader(httpExchange.getRequestBody(),"UTF-8"));
                 String line=null;
                 StringBuilder stringBuilder=new StringBuilder();
+
                 while ((line=in.readLine())!=null){
                     System.out.println(line);
                     stringBuilder.append(line);
                 }
+
                 String params=stringBuilder.toString();
                 Map<String,String> map= JsonObject.parser(params);
+
                 String name=map.get("name");
-                Headers h = t.getResponseHeaders();
                 String response=new Response(name,new Date()).toString();
-                System.out.println(response);
-                h.put("Content-Type", Collections.singletonList("application/json"));
+
                 byte[] bytes = response.getBytes(StandardCharsets.UTF_8);
-                t.sendResponseHeaders(200, bytes.length);
-                OutputStream os = t.getResponseBody();
+
+                Headers headerResponse = httpExchange.getResponseHeaders();
+                headerResponse.put("Content-Type", Collections.singletonList("application/json"));
+
+                httpExchange.sendResponseHeaders(200, bytes.length);
+
+                OutputStream os = httpExchange.getResponseBody();
                 os.write(bytes);
                 os.flush();
                 os.close();
--- src/main/java/util/JsonObject.java
+++ src/main/java/util/JsonObject.java
@@ -8,16 +8,14 @@ public class JsonObject {
     public  static Map<String,String> parser(String json){
         Map<String,String> map=new LinkedHashMap<>();
         String subString=json.substring(1,json.length()-1).trim();
-        String [] splitAttribute=subString.split("\\,+");
+        String [] splitAttribute=subString.split("\\s+\\,\\s+");
         for(String ele:splitAttribute){
-            String []splitEle=ele.split("\"\\:\"");
-            String key=splitEle[0].replaceAll("\"","");
-            String value=splitEle[1].replaceAll("\"","");
+            String []splitEle=ele.split("\\s+\\:\\s+");
+            String key=splitEle[0].trim().replaceAll("\"","");
+            String value=splitEle[1].trim().replaceAll("\"","");
             map.put(key,value);
         }
-        System.out.println(map);
         return  map;
     }
-
 
 }
